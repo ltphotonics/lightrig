5ec0019e12d1a7117379f763e34caa51ef9d4c65
JakeBiele <113914486+JakeBiele@users.noreply.github.com>
2023-07-11 10:06:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ long_description = (this_directory / "README.md").read_text()
 __version__ = "1.0.0"
 
 setuptools.setup(
-    name="ltphotonics",
+    name="lightrig",
     version=__version__,
-    description="Python Library for interfacing with Light Trace Photonics hardware.",
+    description="Python Library for interfacing with Light Trace Photonics Lightrig.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ltphotonics/ltphotonics",
+    url="https://github.com/ltphotonics/lightrig",
     author="Light Trace Photonics Ltd",
     author_email="contact@ltphotonics.co.uk",
-    py_modules=["lightrig"],
+    py_modules=["lightrig","viewer.py"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
